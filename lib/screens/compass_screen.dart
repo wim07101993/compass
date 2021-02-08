3645af345b7bf5deaafd01699e03a02367a1ea86
wim <van.laer.wim@gmail.com>
2021-02-08 22:07:49 +0100

    added compass

--- lib/screens/compass_screen.dart
+++ lib/screens/compass_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 
 class CompassScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Compass')),
+      body: Center(child: _buildCompass()),
+    );
+  }
+
+  Widget _buildCompass() {
+    return StreamBuilder(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        double direction = snapshot.data.heading;
+        return Text('$direction');
+      },
     );
   }
 }
